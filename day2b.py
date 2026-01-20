0f28986e72d5ae249004b3a133270900ef56cff4
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-01-20 09:35:24 +0530

    Create day2b.py

---
+++ day2b.py
@@ -0,0 +1,9 @@
+# Example 3: Comparison operators (==, !=, >, <, >=, <=)
+x = 15
+y = 20
+
+print(x == y)  # Equal: False
+print(x != y)  # Not equal: True
+print(x > y)   # Greater: False
+print(x < y)   # Less: True
+print(x >= 15) # Greater or equal: True
