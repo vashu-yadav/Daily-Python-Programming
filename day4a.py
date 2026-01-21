c6afd048e33a0952ccb8ba59301a804d9a635cad
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-01-22 00:31:24 +0530

    day4 of python programming

---
+++ day4a.py
@@ -0,0 +1,16 @@
+C = '|'
+A = 0
+B = 0
+print(A, B,C, A & B, A | B, A ^ B)
+
+A = 0
+B = 1
+print(A, B,C, A & B, A | B, A ^ B)
+
+A = 1
+B = 0
+print(A, B,C, A & B, A | B, A ^ B)
+
+A = 1
+B = 1
+print(A, B,C, A & B, A | B, A ^ B)
