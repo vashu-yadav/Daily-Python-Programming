9f3a6e078c6160dc7fe76622ea1ca57b63e3ea52
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-01-23 00:01:52 +0530

    Create day5c.py

---
+++ day5c.py
@@ -0,0 +1,15 @@
+integer_var = 10
+float_var = 3.142
+string_var = "Python"
+boolean_var = False
+list_var = [1, 2, 3]
+tuple_var = (1, 2, 3)
+dict_var = {"name": "John", "age": 25}
+
+print("Integer:", integer_var, "| Type:", type(integer_var))
+print("Float:", float_var , "| Type:", type(float_var))
+print("String:", string_var, "| Type:", type(string_var))
+print("Boolean:", boolean_var, "| Type:", type(boolean_var))
+print("List:", list_var, "| Type:", type(list_var))
+print("Tuple:", tuple_var, "| Type:", type(tuple_var))
+print("Dictionary:", dict_var, "| Type:", type(dict_var))
