bead9c3a104eb2860efd6e8ccdfd519c83716102
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-01-28 07:10:14 +0530

    Create day10c.py

---
+++ day10c.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Greatest number is:", a)
+elif b > a:
+    print("Greatest number is:", b)
+else:
+    print("Both numbers are equal")
