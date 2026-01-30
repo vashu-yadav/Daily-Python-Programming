f8f995de7897219ba279ee569cb68c264cea98d9
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-01-30 08:01:28 +0530

    Create day12a.py

---
+++ day12a.py
@@ -0,0 +1,6 @@
+text = "I love C programming and Python coding"
+print(text.replace("C programming", "Python programming"))
+# Output: I love Python programming and Python coding
+
+print(text.replace(" ", "_"))  
+# Output: I_love_C_programming_and_Python_coding
