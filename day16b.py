444dc737659b033a36f8c199d5dbe84d3238680b
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-03 06:43:32 +0530

    Create day16b.py

---
+++ day16b.py
@@ -0,0 +1,11 @@
+year = 2024
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is not a leap year")
+    else:
+        print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
