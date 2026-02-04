f71974584c29eb0fe2b92f2a07c6464c1a2b7a9b
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-04 09:08:16 +0530

    Create day17b.py

---
+++ day17b.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Largest number is:", max(a, b, c))
+print("Smallest number is:", min(a, b, c))
