0b9f3be37814beb0723e5c4202152cf998883eef
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-12 08:43:26 +0530

    Create day25b.py

---
+++ day25b.py
@@ -0,0 +1,12 @@
+# Set operations
+
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+print("Set A:", A)
+print("Set B:", B)
+
+print("Union:", A.union(B))
+print("Intersection:", A.intersection(B))
+print("Difference (A - B):", A.difference(B))
+print("Symmetric Difference:", A.symmetric_difference(B))
