cdfc910a954635f5a4e1f77cb164aabebfcd1ad5
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-12 08:43:31 +0530

    Create day25c.py

---
+++ day25c.py
@@ -0,0 +1,8 @@
+# Remove duplicates using set()
+
+numbers = [10, 20, 30, 20, 40, 10, 50]
+
+unique_numbers = set(numbers)
+
+print("Original List:", numbers)
+print("List after removing duplicates:", unique_numbers)
