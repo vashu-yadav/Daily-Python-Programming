f5c0f7b0c2f3cc551b3c77462c17ee41917e124b
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-13 07:26:59 +0530

    Create day26b.py

---
+++ day26b.py
@@ -0,0 +1,12 @@
+data = {
+    "a": 10,
+    "b": 20,
+    "c": 10,
+    "d": 30,
+    "e": 20
+}
+
+# Use set to remove duplicate values
+unique_values = set(data.values())
+
+print("Unique values in dictionary:", unique_values)
