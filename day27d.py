cf2507eabb3a2b8147ad6cd00336b602b175b23e
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-14 09:03:33 +0530

    Create day27d.py

---
+++ day27d.py
@@ -0,0 +1,28 @@
+
+emp1 = set()
+emp2 = set()
+emp3 = set()
+
+print("Enter 3 employee codes for List 1:")
+for i in range(3):
+    emp1.add(int(input()))
+
+print("Enter 3 employee codes for List 2:")
+for i in range(3):
+    emp2.add(int(input()))
+
+print("Enter 3 employee codes for List 3:")
+for i in range(3):
+    emp3.add(int(input()))
+
+common = emp1 & emp2 & emp3
+
+print("Common Employee Codes:", common)
+
+x=emp1|emp2
+y=emp2|emp3
+
+z= x|y
+a=common-z
+print(a)
+
