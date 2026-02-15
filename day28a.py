7be435614853f57c9dff5c0bebdf6a847313e910
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-16 00:10:02 +0530

    Create day28a.py

---
+++ day28a.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr= np.array([1,2,3,4])
+print(arr)
+
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+print(x + y)   # element-wise addition
+
+
+a = np.array([10, 20, 30])
+
+print(a + 2)
+print(a * 2)
+print(np.sqrt(a))
+print(np.mean(a))
+print(np.max(a))
