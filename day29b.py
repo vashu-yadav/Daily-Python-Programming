a1181f48b22c956f108f722237ee45c261fd25a9
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-18 08:48:49 +0530

    Create day29b.py

---
+++ day29b.py
@@ -0,0 +1,12 @@
+# Program using pop()
+
+car = {
+    "brand": "Honda",
+    "model": "City",
+    "year": 2022
+}
+
+removed_value = car.pop("year")
+
+print("Removed Value:", removed_value)
+print("Updated Dictionary:", car)
