131aaf0e3fcc3153e6a0c54f24081f15ecdc3257
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-18 08:48:51 +0530

    Create day29c.py

---
+++ day29c.py
@@ -0,0 +1,13 @@
+# Program using update()
+
+employee = {
+    "id": 101,
+    "name": "Rahul",
+    "salary": 25000
+}
+
+# Updating dictionary
+employee.update({"salary": 30000})
+employee.update({"department": "IT"})
+
+print(employee)
