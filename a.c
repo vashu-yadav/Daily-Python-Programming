bf98b503da7a917c9640272abfcff0b2ca9be5b0
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-02-21 22:40:54 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a[10];
+    for(int i=0;i<10;i++){
+        
+        a[i]=i;
+    }
+    int *p=&a;
+    for(int i=0;i<10;i++){
+        ++(*p);
+        printf("%d",)
+    
+    }
+
+}
