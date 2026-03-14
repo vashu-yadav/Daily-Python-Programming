dac0c501712e5ba6a53cb852ad5bc75e115cde5a
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-03-14 20:15:56 +0530

    Create day43a.py

---
+++ day43a.py
@@ -0,0 +1,17 @@
+# exceptions.py
+def safe_divide(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero!")
+        return None
+    except (ValueError, TypeError):
+        print("Error: Invalid input!")
+        return None
+    else:
+        return result
+    finally:
+        print("Division attempted.")
+
+print(safe_divide(10, 2))  # 5.0
+print(safe_divide(10, 0))  # Error
