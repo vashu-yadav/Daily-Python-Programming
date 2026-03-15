525377830bdb2f3869c249604e0c862cd4e8fd9a
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-03-16 00:05:03 +0530

    Create day45a.py

---
+++ day45a.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+print(ll.head.data, ll.head.next.data)  # 1 2
