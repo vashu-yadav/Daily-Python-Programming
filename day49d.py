c1e05c58fb8915092b0a76fbd5f96802f5069918
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-03-20 22:40:23 +0530

    Create day49d.py

---
+++ day49d.py
@@ -0,0 +1,8 @@
+# write_file.py
+
+file = open("sample.txt", "w")  # open file in write mode
+file.write("Hello, this is my first file handling program.\n")
+file.write("Learning Python is fun!")
+file.close()
+
+print("Data written successfully.")
