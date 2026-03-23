b2630b90f3ca9b893fafe05cc60ea8a98019b335
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-03-23 23:17:36 +0530

    Create x.py

---
+++ x.py
@@ -0,0 +1,10 @@
+# atharv_ascii.py
+atharv_art = """
+  AAAAA   TTTTTTT  H   H  AAAAA  RRRRR   V   V
+ A     A     T     H   H  A   A  R    R   V V 
+ AAAAAAA     T     HHHHH  AAAAA  RRRRR     V  
+ A     A     T     H   H  A   A  R  R      V   
+ A     A     T     H   H  A   A  R   R     V   
+"""
+
+print(atharv_art)
