6108c014511d10346d68d208dfbdeaaa538af129
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-03-24 22:17:29 +0530

    Create day52d.py

---
+++ day52d.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not Prime")
+else:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime Number")
