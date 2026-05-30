170d7b50285482bb30f1b14433bff74a7a9eed19
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-05-30 17:02:06 +0530

    Create d65.c

---
+++ d65.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    int key = 30;
+
+    int i, found = 0;
+
+    for(i = 0; i < 5; i++){
+
+        if(arr[i] == key){
+
+            printf("Element found at index %d", i);
+
+            found = 1;
+
+            break;
+        }
+    }
+
+    if(found == 0){
+        printf("Element not found");
+    }
+
+    return 0;
+}
