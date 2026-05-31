6d4a3f2adb77a173237a0f241d0ed89bd868f6e8
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-05-31 21:24:40 +0530

    Create d69.c

---
+++ d69.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = 7;
+
+    for(int i = 0; i < n - 1; i++) {
+
+        for(int j = 0; j < n - i - 1; j++) {
+
+            if(arr[j] > arr[j + 1]) {
+
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted Array:\n");
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
