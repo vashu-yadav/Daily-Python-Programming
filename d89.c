3164059978407b6b7f6c11ee6198d944d77488d6
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-06-05 20:49:58 +0530

    Create d89.c

---
+++ d89.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+// Function prototype
+int maxDepth(struct TreeNode* root);
+
+// Function definition
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return (left > right ? left : right) + 1;
+}
